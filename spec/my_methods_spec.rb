4e5623b1d3790eb8ab5e69cc09f6567a6b3544f0
Ashah15 <ashaabdillahi53@gmail.com>
2019-09-17 23:47:32 +0300

    Modify spec/my_methods_spec.rb

--- spec/my_methods_spec.rb
+++ spec/my_methods_spec.rb
@@ -47,4 +47,18 @@ RSpec.describe Enumerable do
     end
   end
 
+  describe '#my_count' do
+    it 'returns the frequency of element in array.return the array size if parameter is not given.' do
+      expect(test_array_2.my_count(2) {}).to eql(4)
+    end
+  end
+
+  describe '#my_inject' do
+    it 'returns the accumulated result of operation performed on each element in
+    the array with initial value given as parameter.' do
+      expect(test_array_2.my_inject(1) { |res, ele| res + ele }).to eql(14)
+    end
+  end
+end
+
   
